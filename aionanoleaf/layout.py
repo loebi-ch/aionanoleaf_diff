85e9a3a5acb9f757f2e526b21c63db199382066e
Andy <andy@slyweb.ch>
2025-11-25 22:21:09 +0100

    Add files via upload

--- aionanoleaf/layout.py
+++ aionanoleaf/layout.py
@@ -1,19 +1,22 @@
 # Copyright 2021, Milan Meulemans.
+# Modified and optimized 2025 by loebi-ch
+# Added support for Nanoleaf Essentials based on the work of JaspervRijbroek in 2025
+# Added support for 4D/Screen Mirroring emersion modes (1D, 2D, 3D, 4D) based on the work of jonathanrobichaud4 in 2024
 #
-# This file is part of aionanoleaf.
+# This file is part of aionanoleaf2, the refactored version of aionanoleaf by Milan Meulemans
 #
-# aionanoleaf is free software: you can redistribute it and/or modify
+# aionanoleaf2 is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# aionanoleaf is distributed in the hope that it will be useful,
+# aionanoleaf2 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with aionanoleaf.  If not, see <https://www.gnu.org/licenses/>.
+# along with aionanoleaf2.  If not, see <https://www.gnu.org/licenses/>.
 
 """Nanoleaf layout."""
 from __future__ import annotations
